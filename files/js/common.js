87ff2b843a2571a4eb0b9eadec3d022bb1abe567
Олексій Чечель <alex.mirrr@gmail.com>
2017-01-24 11:30:30 +0700

    Minor changes and fixes

--- files/js/common.js
+++ files/js/common.js
@@ -1,8 +1,19 @@
-$(function () {
-	window.createWebSocket = function (path) {
-		return new WebSocket((location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + path);
-	}
+function createWebSocket(path) {
+	return new WebSocket((location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host + path);
+}
 
+Number.prototype.formatMoney = function (c, d, t) {
+	var n = this,
+		c = isNaN(c = Math.abs(c)) ? 2 : c,
+		d = d == undefined ? "." : d,
+		t = t == undefined ? "&nbsp;" : t,
+		s = n < 0 ? "-" : "",
+		i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+		j = (j = i.length) > 3 ? j % 3 : 0;
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+$(function () {
 	var pixelRatio = (window && window.devicePixelRatio || 1);
 
 	if (pixelRatio > 1.5) {
@@ -108,17 +119,6 @@ $(function () {
 		});
 	});
 
-
-	Number.prototype.formatMoney = function (c, d, t) {
-		var n = this,
-			c = isNaN(c = Math.abs(c)) ? 2 : c,
-			d = d == undefined ? "." : d,
-			t = t == undefined ? "&nbsp;" : t,
-			s = n < 0 ? "-" : "",
-			i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
-			j = (j = i.length) > 3 ? j % 3 : 0;
-		return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-	};
 
 	// выделение текста при клике
 	$('body').on('click', '.clickselect', function (event) {
--- files/js/vendor/jquery.tabs.js
+++ files/js/vendor/jquery.tabs.js
@@ -1,29 +1,31 @@
 $(function () {
 	// Вкладки и табы
-	$('div.tabs>*').forceClick(function (event) {
-		var $tabs = $(this).parent();
+	$('div.tabs>*[data-id]').forceClick(function (event) {
+		if ($(this).not('[disabled]').not('.disabled').length > 0) {
+			var $tabs = $(this).parent();
 
-		if ($tabs.attr('multiple')) {
-			if (!event.ctrlKey) {
+			if ($tabs.attr('multiple')) {
+				if (!event.ctrlKey) {
+					$tabs.children().removeClass('active');
+					$(this).addClass('active');
+				} else {
+					$(this).toggleClass('active');
+				}
+
+				var arr = [];
+				$tabs.children().each(function () {
+					if ($(this).hasClass('active')) {
+						arr.push($(this).data('id'));
+					}
+				});
+				$tabs.data('active', arr);
+			} else {
 				$tabs.children().removeClass('active');
 				$(this).addClass('active');
-			} else {
-				$(this).toggleClass('active');
+				$tabs.data('active', [$(this).data('id')]);
 			}
 
-			var arr = [];
-			$tabs.children().each(function () {
-				if ($(this).hasClass('active')) {
-					arr.push($(this).data('id'));
-				}
-			});
-			$tabs.data('active', arr);
-		} else {
-			$tabs.children().removeClass('active');
-			$(this).addClass('active');
-			$tabs.data('active', [$(this).data('id')]);
+			$tabs.trigger('change');
 		}
-
-		$tabs.trigger('change');
 	});
 });
