916553c15ef7e8a8b10ddeda1c32f920be3db451
Олексій Чечель <alex.mirrr@gmail.com>
2017-02-27 00:18:17 +0700

    Update summerGen version

--- summerGen/summerGen.go
+++ summerGen/summerGen.go
@@ -10,7 +10,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "summerGen"
 	app.Author = "Oleksiy Chechel (alex.mirrr@gmail.com)"
-	app.Version = "1.6.14"
+	app.Version = "1.6.15"
 	app.Commands = []cli.Command{
 		cli.Command{
 			Name:   "module",
