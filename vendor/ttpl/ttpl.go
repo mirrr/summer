998e16b049e543e54db413bc51be51990d30a853
Oleksiy Chechel <alex.mirrr@gmail.com>
2019-02-05 00:42:25 +0200

    Small fix

--- vendor/ttpl/ttpl.go
+++ vendor/ttpl/ttpl.go
@@ -110,8 +110,7 @@ func (r PageRender) Render(w http.ResponseWriter) error {
 		"o": func(key string) interface{} {
 			if lang, ok := site["lang"]; ok && len(site["lang"].([]string)) > 0 {
 				if translate, ok1 := r.funcMap["translate"]; ok1 {
-					if translate2, ok2 := translate.(func(module, key, lang string) interface{}); ok2 {
-						fmt.Println(translate, lang.([]string)[0])
+					if translate2, ok2 := translate.(func(module, key, lang string) string); ok2 {
 						return translate2(site["module"].(string), key, lang.([]string)[0])
 					}
 				}
